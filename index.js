33fb817eb808c4eabc8e068f88c7d928a7de6f4f
PRP <prp9812@gmail.com>
2018-11-04 22:29:40 +0530

    Implement typing event

--- index.js
+++ index.js
@@ -17,5 +17,8 @@ io.on('connection', function (socket){
   console.log('New user has connected', socket.id);
   socket.on('chat', function(data){
     io.sockets.emit('chat', data);
-  })
+  });
+  socket.on('typing', function(data){
+    socket.broadcast.emit('typing', data);
+  });
 });
--- public/chat.js
+++ public/chat.js
@@ -6,6 +6,7 @@ var message = document.getElementById('message');
 var handle = document.getElementById('handle');
 var btn = document.getElementById('send');
 var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
 
 // Emit Events
 btn.addEventListener('click', function(){
@@ -15,7 +16,15 @@ btn.addEventListener('click', function(){
   })
 });
 
+message.addEventListener('keypress', function(){
+  socket.emit('typing', handle.value);
+});
 // Listen for Events
 socket.on('chat', function(data){
+  feedback.innerHTML = '';
   output.innerHTML += '<p><strong>' + data.handle+ '</strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+  feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
